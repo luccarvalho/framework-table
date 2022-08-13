1217fc930623b155f6d9e117fd98cd7926a04986
Lucas <lucasalexandre_82@hotmail.com>
2022-08-13 13:10:54 -0300

    Criando uma instância de negociação

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -1,14 +1,24 @@
 class NegociacaoController {
+
+    constructor() {
+        let $ = document.querySelector.bind(document);
+        this._inputData = $("#data");
+        this._inputQuantidade = $("#quantidade");
+        this._inputValor = $("#valor");
+    }
+
     adiciona(event) {
+
         event.preventDefault();
 
-        let $ = document.querySelector.bind(document);
-        let inputData = $("#data");
-        let inputQuantidade = $("#quantidade");
-        let inputValor = $("#valor");
+        console.log(typeof (this._inputData.value));
 
-        console.log(inputData.value);
-        console.log(inputQuantidade.value);
-        console.log(inputValor.value);
+        let negociacao = new Negociacao(
+            this._inputData.value,
+            this._inputQuantidade.value,
+            this._inputValor.value
+        );
+
+        console.log(negociacao);
     }
 }
