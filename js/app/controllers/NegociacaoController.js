ba9998841cd58869ebe5400fa97b76755dc68ba0
Lucas <lucasalexandre_82@hotmail.com>
2022-08-15 20:15:13 -0300

    Alterando código da classe controllers e criando uma classe nova na pasta helpers

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -11,14 +11,15 @@ class NegociacaoController {
 
         event.preventDefault();
 
-        let data = new Date(...this._inputData.value.split("-").map((item, indice) => item - indice % 2));
+        let helper = new DateHelper();
 
         let negociacao = new Negociacao(
-            data,
+            helper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
             this._inputValor.value
         );
 
         console.log(negociacao);
+        console.log(helper.dataParaTexto(negociacao.data));
     }
 }
